c3485bfe0a24a270e9b1b9ff76b8b0e68b2acca0
jerseyjack21 <jerseyjack21@gmail.com>
2016-02-16 19:45:13 -0600

    first commit dude

--- js/script.js
+++ js/script.js
@@ -1,9 +1,52 @@
 $(document).ready(function() {
-    console.log("Hello world.")
+    getData();
+
+
 });
 
 
 
+
+
+
+
+function getData() {
+
+	$.getJSON("js/columbia.json", function(schoolsData){
+
+
+		loopThroughData(schoolsData);
+	})
+
+}
+
+
+function loopThroughData(s) {
+
+	for (i=0; i<s.length; i++){
+
+		var schoolName = s[i]["SCHOOL_NAME"];
+		var year = s[i]["YEAR"];
+		var gradPct = s[i]["Graduates_PCT"];
+
+		
+
+			1 === 1;
+			1 == "1";
+			1 === "1";
+
+			if (schoolName === "FREDERICK DOUGLASS HIGH"){
+
+				$(".chart").append("div class='bar'style='width: "+gradPct+"px'></div>");
+
+
+
+					
+			}
+		
+	}
+
+}
 
 
 
